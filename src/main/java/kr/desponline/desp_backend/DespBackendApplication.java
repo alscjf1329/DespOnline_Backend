d06bf4bb453fd19ebaf5c983862115ab7166bc5f
wonindev <sangwonparkdev@gmail.com>
2023-11-17 15:25:05 +0900

    set up

---
+++ src/main/java/kr/desponline/desp_backend/DespBackendApplication.java
@@ -0,0 +1,13 @@
+package kr.desponline.desp_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DespBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DespBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/desponline/desp_backend/DespBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.desponline.desp_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DespBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
