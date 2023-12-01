201c351c9b94e09cf007c5db88922868dc5202fe
wonindev <sangwonparkdev@gmail.com>
2023-12-01 19:37:03 +0900

    다시 RestController로 변경

--- src/main/java/kr/desponline/desp_backend/controller/CashChargeController.java
+++ src/main/java/kr/desponline/desp_backend/controller/CashChargeController.java
@@ -7,12 +7,9 @@ import kr.desponline.desp_backend.service.CashChargeLogService;
 import kr.desponline.desp_backend.service.SearchService;
 import kr.desponline.desp_backend.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 @RequestMapping("/cash")
 public class CashChargeController {
     private static final UserEntity NOT_EXIST_USER = null;
@@ -29,28 +26,28 @@ public class CashChargeController {
     }
 
     @GetMapping("/existuser/{nickname}")
-    public ResponseEntity isExistUser(@PathVariable String nickname) {
+    public boolean isExistUser(@PathVariable String nickname) {
         String uuid = searchService.findUuidByNickname(nickname);
         if (uuid == null) {
-            return new ResponseEntity<>(false, HttpStatus.OK);
+            return false;
         }
         UserEntity user = userService.findUserEntityByUuid(uuid);
         if (user == NOT_EXIST_USER) {
-            return new ResponseEntity<>(false, HttpStatus.OK);
+            return false;
         }
-        return new ResponseEntity<>(true, HttpStatus.OK);
+        return true;
     }
 
     @GetMapping("/existcash/{money}")
-    public ResponseEntity isExistCash(@PathVariable String money) {
+    public boolean isExistCash(@PathVariable String money) {
         if (money.matches("\\d+")) {
-            return new ResponseEntity<>(Integer.parseInt(money) < 100000, HttpStatus.OK);
+            return Integer.parseInt(money) < 100000;
         }
-        return new ResponseEntity<>(false, HttpStatus.OK);
+        return false;
     }
 
     @PutMapping("/charge")
-    public String chargeCash(@RequestBody CashChargeDTO cashChargeDTO) {
+    public boolean chargeCash(@RequestBody CashChargeDTO cashChargeDTO) {
         String uuid = searchService.findUuidByNickname(cashChargeDTO.getNick_name());
         UserEntity user = userService.findUserEntityByUuid(uuid);
         int cash = Integer.parseInt(cashChargeDTO.getAmount());
@@ -60,14 +57,14 @@ public class CashChargeController {
         System.out.println("legacyCash + cash: " + (legacyCash + cash));
         user.setCash(legacyCash + cash);
         userService.updateUser(user);
-        return "redirect:/";
+        return true;
     }
 
     @PostMapping("/chargelog")
-    public ResponseEntity createChargeLog(@RequestBody CashChargeLogDTO cashChargeLogDTO) {
+    public boolean createChargeLog(@RequestBody CashChargeLogDTO cashChargeLogDTO) {
         String nickname = cashChargeLogDTO.getNick_name();
         String uuid = searchService.findUuidByNickname(nickname);
         cashChargeLogService.createCashChargeLog(cashChargeLogDTO, uuid);
-        return new ResponseEntity<>(true, HttpStatus.OK);
+        return true;
     }
 }
