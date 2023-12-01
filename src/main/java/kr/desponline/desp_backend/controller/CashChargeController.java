6a39177c281e5526028c3916064f2c5eb1275238
wonindev <sangwonparkdev@gmail.com>
2023-12-01 17:55:53 +0900

    RestController -> Controller로 변경하고 return을 다 json형태로 변경, 캐쉬 충전 redirect 홈화면으로 변경

--- src/main/java/kr/desponline/desp_backend/controller/CashChargeController.java
+++ src/main/java/kr/desponline/desp_backend/controller/CashChargeController.java
@@ -11,7 +11,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 @RequestMapping("/cash")
@@ -51,7 +50,7 @@ public class CashChargeController {
     }
 
     @PutMapping("/charge")
-    public RedirectView chargeCash(@RequestBody CashChargeDTO cashChargeDTO) {
+    public String chargeCash(@RequestBody CashChargeDTO cashChargeDTO) {
         String uuid = searchService.findUuidByNickname(cashChargeDTO.getNick_name());
         UserEntity user = userService.findUserEntityByUuid(uuid);
         int cash = Integer.parseInt(cashChargeDTO.getAmount());
@@ -61,16 +60,14 @@ public class CashChargeController {
         System.out.println("legacyCash + cash: " + (legacyCash + cash));
         user.setCash(legacyCash + cash);
         userService.updateUser(user);
-        RedirectView redirectView = new RedirectView();
-        redirectView.setUrl("redirect:/");
-        return redirectView;
+        return "redirect:/";
     }
 
     @PostMapping("/chargelog")
-    public boolean createChargeLog(@RequestBody CashChargeLogDTO cashChargeLogDTO) {
+    public ResponseEntity createChargeLog(@RequestBody CashChargeLogDTO cashChargeLogDTO) {
         String nickname = cashChargeLogDTO.getNick_name();
         String uuid = searchService.findUuidByNickname(nickname);
         cashChargeLogService.createCashChargeLog(cashChargeLogDTO, uuid);
-        return true;
+        return new ResponseEntity<>(true, HttpStatus.OK);
     }
 }
