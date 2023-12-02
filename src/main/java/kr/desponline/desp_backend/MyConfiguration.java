4d3109cacc3d353be149312db2d20781c6e546c9
wonindev <sangwonparkdev@gmail.com>
2023-12-02 19:05:34 +0900

    Feat: MyConfiguration 추가 cors 해결

---
+++ src/main/java/kr/desponline/desp_backend/MyConfiguration.java
@@ -0,0 +1,17 @@
+package kr.desponline.desp_backend;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class MyConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
+}
+
