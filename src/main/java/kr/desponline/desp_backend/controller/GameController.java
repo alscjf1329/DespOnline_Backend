f6b8f24ba8a048e016dd407aae819340050c03eb
alscjf1329 <alscjf1329@gmail.com>
2024-01-31 03:02:28 +0900

    fix) application.properties에서 참조하게끔 다시 수정

--- src/main/java/kr/desponline/desp_backend/controller/GameController.java
+++ src/main/java/kr/desponline/desp_backend/controller/GameController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/game")
 public class GameController {
 
-    @Value("frontend.login-page-url")
+    @Value("${frontend.login-page-url}")
     private String loginEndpoint;
 
     @PostMapping("/main")
