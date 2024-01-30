aae738532ca20ffedffd5ee3b6d17d8e52155ea1
alscjf1329 <alscjf1329@gmail.com>
2024-01-31 02:40:32 +0900

    fix) cookie 없어도 정상작동하도록 수정

--- src/main/java/kr/desponline/desp_backend/controller/GameController.java
+++ src/main/java/kr/desponline/desp_backend/controller/GameController.java
@@ -3,7 +3,6 @@ package kr.desponline.desp_backend.controller;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import java.io.IOException;
-import java.util.List;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,19 +12,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/game")
 public class GameController {
+
     @Value("frontend.login-page-url")
     private String loginEndpoint;
 
     @PostMapping("/main")
     public boolean getGameInfo(
-        @CookieValue(AuthController.SESSION_KEY_COOKIE_NAME) String sessionKey,
+        @CookieValue(name = AuthController.SESSION_KEY_COOKIE_NAME, required = false, defaultValue = "") String sessionKey,
         HttpSession session,
         HttpServletResponse response) throws IOException {
-        if (session.getAttribute(sessionKey) == null) {
+        if (sessionKey.isEmpty() || session.getAttribute(sessionKey) == null) {
             response.sendRedirect(loginEndpoint);
             return false;
         }
         return true;
     }
+
 
 }
