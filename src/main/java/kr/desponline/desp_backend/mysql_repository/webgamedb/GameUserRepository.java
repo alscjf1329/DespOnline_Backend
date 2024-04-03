74118101c80403100f280858ec164ded1e965243
alscjf1329 <alscjf1329@gmail.com>
2024-04-03 23:07:23 +0900

    fix) GameUser entity 구조가 바뀜으로 인한 버그 수정
    GameUser entity 컬럼에 nickname이 없어졌기에 uuid로 변경하였습니다.

--- src/main/java/kr/desponline/desp_backend/mysql_repository/webgamedb/GameUserRepository.java
+++ src/main/java/kr/desponline/desp_backend/mysql_repository/webgamedb/GameUserRepository.java
@@ -1,13 +1,12 @@
 package kr.desponline.desp_backend.mysql_repository.webgamedb;
 
 import kr.desponline.desp_backend.entity.webgamedb.GameUserEntity;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface GameUserRepository extends JpaRepository<GameUserEntity, Long> {
 
-    GameUserEntity findGameUserEntityByNickname(String nickname);
+    GameUserEntity findGameUserEntityByUuid(String uuid);
 
 }
