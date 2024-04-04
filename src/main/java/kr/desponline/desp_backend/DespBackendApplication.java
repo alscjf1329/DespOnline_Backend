6102e63a37ecefc4c00ffe20c51b3c707523adf6
alscjf1329 <alscjf1329@gmail.com>
2024-04-04 22:18:11 +0900

    fix) spring boot security login page 삭제
    spring boot security 의존성을 추가하게 되면 자동으로 추가되는 login page를 삭제합니다.

--- src/main/java/kr/desponline/desp_backend/DespBackendApplication.java
+++ src/main/java/kr/desponline/desp_backend/DespBackendApplication.java
@@ -2,9 +2,10 @@ package kr.desponline.desp_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 @EnableReactiveMongoRepositories
 public class DespBackendApplication {
     public static void main(String[] args) {
