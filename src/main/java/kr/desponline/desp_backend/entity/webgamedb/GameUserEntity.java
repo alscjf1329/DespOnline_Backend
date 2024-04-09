efe6e3ebb5f5223ecc8445bc78359fcd6a26d4a0
alscjf1329 <alscjf1329@gmail.com>
2024-04-09 16:04:05 +0900

    refactor) GameUserEntity 스키마 변경
    role, state 타입을 String으로 변경했습니다.

--- src/main/java/kr/desponline/desp_backend/entity/webgamedb/GameUserEntity.java
+++ src/main/java/kr/desponline/desp_backend/entity/webgamedb/GameUserEntity.java
@@ -1,6 +1,9 @@
 package kr.desponline.desp_backend.entity.webgamedb;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import java.time.LocalDateTime;
@@ -21,15 +24,34 @@ public class GameUserEntity {
 
     @Id
     private String uuid;
+
     private String id;
-    private String encoded_password;
-    private Integer role;
-    private Integer state;
-    private LocalDateTime created_at;
-    private LocalDateTime last_login_at;
+
+    @Column(name = "encoded_password")
+    private String encodedPassword;
+
+    @Enumerated(EnumType.STRING)
+    private ROLE role;
+
+    @Enumerated(EnumType.STRING)
+    private STATE state;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "last_login_at")
+    private LocalDateTime lastLoginAt;
 
     public static GameUserEntity createUser(String uuid, String id, String encodedPassword) {
-        return new GameUserEntity(uuid, id, encodedPassword, 2, 1,
+        return new GameUserEntity(uuid, id, encodedPassword, ROLE.USER, STATE.ACTIVATED,
             LocalDateTime.now(), null);
+    }
+
+    public enum ROLE {
+        ADMIN, STAFF, USER
+    }
+
+    public enum STATE {
+        ACTIVATED, DEACTIVATED, BANNED, STOPPED, REMOVED
     }
 }
