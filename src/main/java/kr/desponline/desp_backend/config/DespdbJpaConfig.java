15f48dc76a934e60630494e096c0b0665fdc4d02
alscjf1329 <alscjf1329@gmail.com>
2024-04-11 23:16:44 +0900

    refactor) JPA config 정리
    DataSourceConfig에 있던 DB 설정들을 각각의 클래스로 분배하여 정리했습니다.

--- src/main/java/kr/desponline/desp_backend/config/DespdbJpaConfig.java
+++ src/main/java/kr/desponline/desp_backend/config/DespdbJpaConfig.java
@@ -1,7 +1,19 @@
 package kr.desponline.desp_backend.config;
 
+import com.zaxxer.hikari.HikariDataSource;
+import java.util.HashMap;
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
 @EnableJpaRepositories(basePackages = "kr.desponline.desp_backend.mysql_repository.despdb",
@@ -9,4 +21,40 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
     transactionManagerRef = "despdbTransactionManager")
 public class DespdbJpaConfig {
 
+    @Bean
+    @Primary
+    @ConfigurationProperties(prefix = "spring.despdb")
+    public DataSource despdbDataSource() {
+        return DataSourceBuilder.create().type(HikariDataSource.class).build();
+    }
+
+    @Primary
+    @Bean(name = "despdbEntityManagerFactory")
+    public LocalContainerEntityManagerFactoryBean despdbEntityManagerFactory(
+        @Qualifier("despdbDataSource") DataSource dataSource) {
+        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+        em.setDataSource(dataSource);
+        em.setPackagesToScan("kr.desponline.desp_backend.entity.despdb"); // despdb 엔티티 패키지 위치
+
+        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        vendorAdapter.setShowSql(true);
+        em.setJpaVendorAdapter(vendorAdapter);
+
+        HashMap<String, Object> prop = new HashMap<>();
+        prop.put("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
+        prop.put("hibernate.hbm2ddl.auto", "none");
+        prop.put("hibernate.format_sql", true);
+        em.setJpaPropertyMap(prop);
+        return em;
+    }
+
+    @Primary
+    @Bean
+    public PlatformTransactionManager despdbTransactionManager(
+        @Qualifier("despdbEntityManagerFactory") LocalContainerEntityManagerFactoryBean despdbEntityManagerFactory) {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(despdbEntityManagerFactory.getObject());
+        transactionManager.setTransactionSynchronization(JpaTransactionManager.SYNCHRONIZATION_ON_ACTUAL_TRANSACTION);
+        return transactionManager;
+    }
 }
--- src/main/java/kr/desponline/desp_backend/config/WebgamedbJpaConfig.java
+++ src/main/java/kr/desponline/desp_backend/config/WebgamedbJpaConfig.java
@@ -1,11 +1,56 @@
 package kr.desponline.desp_backend.config;
 
+import com.zaxxer.hikari.HikariDataSource;
+import java.util.HashMap;
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
 @Configuration
 @EnableJpaRepositories(basePackages = "kr.desponline.desp_backend.mysql_repository.webgamedb",
     entityManagerFactoryRef = "webgamedbEntityManagerFactory",
     transactionManagerRef = "webgamedbTransactionManager")
 public class WebgamedbJpaConfig {
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.webgamedb")
+    public DataSource webgamedbDataSource() {
+        return DataSourceBuilder.create().type(HikariDataSource.class).build();
+    }
+
+
+    // webgamedb에 대한 EntityManagerFactory 설정
+    @Bean(name = "webgamedbEntityManagerFactory")
+    public LocalContainerEntityManagerFactoryBean webgamedbEntityManagerFactory(
+        @Qualifier("webgamedbDataSource") DataSource dataSource) {
+        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+        em.setDataSource(dataSource);
+        em.setPackagesToScan("kr.desponline.desp_backend.entity.webgamedb"); // webgamedb 엔티티 패키지 위치
+
+        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        vendorAdapter.setShowSql(true);
+        em.setJpaVendorAdapter(vendorAdapter);
+
+        HashMap<String, Object> prop = new HashMap<>();
+        prop.put("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
+        prop.put("hibernate.hbm2ddl.auto", "none");
+        prop.put("hibernate.format_sql", true);
+        em.setJpaPropertyMap(prop);
+        return em;
+    }
+
+    @Bean
+    public JpaTransactionManager webgamedbTransactionManager(
+        @Qualifier("webgamedbEntityManagerFactory") LocalContainerEntityManagerFactoryBean webgamedbEntityManagerFactory) {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(webgamedbEntityManagerFactory.getObject());
+        return transactionManager;
+    }
 }
