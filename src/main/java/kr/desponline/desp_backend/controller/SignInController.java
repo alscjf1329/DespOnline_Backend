117cd8251f717e283687c859adb267117e1bb7d1
alscjf1329 <alscjf1329@gmail.com>
2024-04-13 16:31:48 +0900

    feat) 로그인 API 추가
    로그인 하는 API(endpoint: /signin)를 추가했습니다.

---
+++ src/main/java/kr/desponline/desp_backend/controller/SignInController.java
@@ -0,0 +1,37 @@
+package kr.desponline.desp_backend.controller;
+
+import jakarta.servlet.http.HttpServletResponse;
+import kr.desponline.desp_backend.dto.SigninRequestDTO;
+import kr.desponline.desp_backend.entity.webgamedb.GameUserEntity;
+import kr.desponline.desp_backend.service.SigninService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/signin")
+public class SignInController {
+
+    private final SigninService signinService;
+
+    @Autowired
+    public SignInController(SigninService signinService) {
+        this.signinService = signinService;
+    }
+
+    @PostMapping("")
+    public ResponseEntity<Object> signIn(@RequestBody SigninRequestDTO signInRequestDTO,
+        HttpServletResponse response) {
+        GameUserEntity gameUser = signinService.signin(signInRequestDTO);
+        if (gameUser == null) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+        String sessionKey = signinService.addSession(gameUser);
+        signinService.addSessionKeyCookie(response, sessionKey);
+        return ResponseEntity.ok().build();
+    }
+}
