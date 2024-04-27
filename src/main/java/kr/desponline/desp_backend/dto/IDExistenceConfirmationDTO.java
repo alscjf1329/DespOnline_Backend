65571358f5d91be49fa926d7267cb21628fe17c8
alscjf1329 <alscjf1329@gmail.com>
2024-04-28 00:15:34 +0900

    fix) /exists/id endpoint 발생
    no argument 생성자 없을시 발생하는 에러 해결했습니다.

--- src/main/java/kr/desponline/desp_backend/dto/IDExistenceConfirmationDTO.java
+++ src/main/java/kr/desponline/desp_backend/dto/IDExistenceConfirmationDTO.java
@@ -1,10 +1,8 @@
 package kr.desponline.desp_backend.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
-@AllArgsConstructor
 public class IDExistenceConfirmationDTO {
 
     private String id;
