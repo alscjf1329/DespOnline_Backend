793655972d8685949a104ae02b4d4296832302b5
alscjf1329 <alscjf1329@gmail.com>
2024-05-06 22:08:53 +0900

    config) 자격 증명 정보(쿠키, HTTP 인증 등)를 요청에 포함 하기 위한 cors 관련 설정 추가
    allowedOrigins: 해당 도메인에서의 요청 허용 설정
    allowCredentials: 자격 증명 정보를 요청에 포함 설정

--- src/main/java/kr/desponline/desp_backend/config/MyConfiguration.java
+++ src/main/java/kr/desponline/desp_backend/config/MyConfiguration.java
@@ -1,5 +1,6 @@
 package kr.desponline.desp_backend.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -9,12 +10,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 public class MyConfiguration implements WebMvcConfigurer {
 
+    @Value("${front.domain}")
+    private String front;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("*")
-            .allowedMethods("*")
-            .allowedHeaders("*");
+            .allowedOrigins(front) // 프론트엔드 도메인에서의 요청을 허용
+            .allowedMethods("*") // http 모든 메소드 요청 허용
+            .allowedHeaders("*") // 헤더 정보 모두 허용
+            .allowCredentials(true);// 쿠키, 세션 정보도 허용
     }
 }
 
