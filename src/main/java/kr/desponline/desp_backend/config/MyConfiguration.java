b55cd8bc88f480138d5e09e33a77fb598a61dfcb
alscjf1329 <alscjf1329@gmail.com>
2024-05-06 22:44:07 +0900

    config) 허용 프론트 도메인을 배열로 받게 수정

--- src/main/java/kr/desponline/desp_backend/config/MyConfiguration.java
+++ src/main/java/kr/desponline/desp_backend/config/MyConfiguration.java
@@ -10,13 +10,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 public class MyConfiguration implements WebMvcConfigurer {
 
-    @Value("${front.domain}")
-    private String front;
+    @Value("${front.domains}")
+    private String[] frontDomains;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins(front) // 프론트엔드 도메인에서의 요청을 허용
+            .allowedOrigins(frontDomains) // 프론트엔드 도메인에서의 요청을 허용
             .allowedMethods("*") // http 모든 메소드 요청 허용
             .allowedHeaders("*") // 헤더 정보 모두 허용
             .allowCredentials(true);// 쿠키, 세션 정보도 허용
