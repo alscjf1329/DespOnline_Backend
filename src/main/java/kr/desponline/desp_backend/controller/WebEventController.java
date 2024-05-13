4ea9665ff045e255fd22c97d22b4fd533420da30
alscjf1329 <alscjf1329@gmail.com>
2024-05-13 21:00:58 +0900

    feat) webEvent 관련 controller 생성
    "/event": 진행 중인 이벤트를 불러오는 endpoint
    
    "/event/cardFlipping/{eventId}": 해당 유저의 이벤트 정보를 불러오는 endpoint

---
+++ src/main/java/kr/desponline/desp_backend/controller/WebEventController.java
@@ -0,0 +1,72 @@
+package kr.desponline.desp_backend.controller;
+
+import kr.desponline.desp_backend.dto.EventUserInfoResponseDTO;
+import kr.desponline.desp_backend.dto.web_event.CardFlippingDTO;
+import kr.desponline.desp_backend.entity.mongodb.web_event.WebEventEntity;
+import kr.desponline.desp_backend.entity.mysql.webgamedb.GameUserEntity;
+import kr.desponline.desp_backend.service.SigninSessionService;
+import kr.desponline.desp_backend.service.WebEventService;
+import kr.desponline.desp_backend.service.web_event.CardFlippingService;
+import kr.desponline.desp_backend.strategy.RandomIntegerListStrategy;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/event")
+public class WebEventController {
+
+    private final WebEventService webEventService;
+    private final SigninSessionService signinSessionService;
+    private final CardFlippingService cardFlippingService;
+    private final RandomIntegerListStrategy randomIntegerListStrategy;
+
+    @Autowired
+    public WebEventController(
+        WebEventService webEventService,
+        SigninSessionService signinSessionService,
+        CardFlippingService cardFlippingService,
+        RandomIntegerListStrategy randomIntegerListStrategy) {
+        this.webEventService = webEventService;
+        this.signinSessionService = signinSessionService;
+        this.cardFlippingService = cardFlippingService;
+        this.randomIntegerListStrategy = randomIntegerListStrategy;
+    }
+
+    @GetMapping("")
+    public ResponseEntity<?> findAllEventInPeriod() {
+        return ResponseEntity.ok().body(webEventService.findAllInPeriod());
+    }
+
+    @GetMapping("/cardFlipping/{eventId}")
+    public ResponseEntity<EventUserInfoResponseDTO> showCardFlippingEvent(
+        @CookieValue(value = SigninSessionService.SESSION_KEY_COOKIE_NAME, required = false) String sessionKey,
+        @PathVariable("eventId") String eventId
+    ) {
+        if (sessionKey == null) {
+            return ResponseEntity.status(HttpStatus.MOVED_PERMANENTLY).build();
+        }
+
+        GameUserEntity gameUser = signinSessionService.findSession(sessionKey);
+        if (gameUser == null) {
+            return ResponseEntity.status(HttpStatus.MOVED_PERMANENTLY).build();
+        }
+        CardFlippingDTO cardFlippingDTO = cardFlippingService.findByUserUuidAndEventId(
+            gameUser.getUuid(), eventId);
+
+        if (cardFlippingDTO == null) {
+            WebEventEntity event = webEventService.findById(eventId);
+            CardFlippingDTO defaultCardFlippingDTO = CardFlippingDTO.createDefault(
+                gameUser, event, randomIntegerListStrategy);
+            cardFlippingService.save(defaultCardFlippingDTO);
+        }
+
+        return ResponseEntity.ok()
+            .body(new EventUserInfoResponseDTO(webEventService.findById(eventId), cardFlippingDTO));
+    }
+}
